38aebb35e29308ff7d442b60a534ba7dc01cc676
Hibi_10000 <87810571+Hibi-10000@users.noreply.github.com>
2024-04-13 22:56:04 +0900

    refactor: all

--- src/main/java/com/github/hibi_10000/plugins/SidebarEconomy.java
+++ src/main/java/com/github/hibi_10000/plugins/SidebarEconomy.java
@@ -1,8 +1,6 @@
 package com.github.hibi_10000.plugins;
 
-import java.util.ArrayList;
-import java.util.List;
-
+import net.milkbowl.vault.economy.Economy;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
@@ -21,96 +19,52 @@ import org.bukkit.scoreboard.Objective;
 import org.bukkit.scoreboard.Score;
 import org.bukkit.scoreboard.Scoreboard;
 
-import net.milkbowl.vault.economy.Economy;
+import java.util.ArrayList;
+import java.util.List;
 
 public class SidebarEconomy extends JavaPlugin implements Listener {
     boolean enable = true;
     BukkitTask task = null;
     Economy econ = null;
-    List<String> players = new ArrayList<String>();
+    final List<String> players = new ArrayList<>();
 
-    @SuppressWarnings("deprecation")
     @Override
     public void onEnable() {
         super.onEnable();
         task = new BukkitRunnable() {
-            List<String> players = new ArrayList<String>();
             @Override
             public void run() {
-                for (Player p : Bukkit.getOnlinePlayers()) {
-                    p.setScoreboard(Bukkit.getScoreboardManager().getNewScoreboard());
-                    Scoreboard board;
-                    board = Bukkit.getScoreboardManager().getNewScoreboard();
-                    Objective obj = board.registerNewObjective(p.getName(), "dummy");
-                    obj.setDisplaySlot(DisplaySlot.SIDEBAR);
-                    obj.setDisplayName(ChatColor.GOLD + "Money");
-                    Score score = obj.getScore(p.getName());
-                    score.setScore((int) econ.getBalance(p.getName()));
-                    p.setScoreboard(board);
-                    this.players.add(p.getName());
-                    if (this.players.contains(p.getName())) {
-                        if(enable == true) {
-                            Score score1 = obj.getScore(p);
-                            score1.setScore((int) econ.getBalance(p.getName()));
-                            p.setScoreboard(board);
-                        } else {
-                            board.resetScores(p.getName());
-                        }
-                    }
-                }
+                updateScoreboard();
             }
         }.runTaskTimer(this, 0L, 20L);
         getServer().getPluginManager().registerEvents(this, this);
         setupEconomy();
+        updateScoreboard();
+    }
+
+    @EventHandler
+    public void onJoin(PlayerJoinEvent e) {
+        updateScoreboard();
+    }
+
+    @SuppressWarnings("deprecation")
+    public void updateScoreboard() {
         for (Player p : Bukkit.getOnlinePlayers()) {
             p.setScoreboard(Bukkit.getScoreboardManager().getNewScoreboard());
-            Scoreboard board;
-            board = Bukkit.getScoreboardManager().getNewScoreboard();
+            Scoreboard board = Bukkit.getScoreboardManager().getNewScoreboard();
             Objective obj = board.registerNewObjective(p.getName(), "dummy");
             obj.setDisplaySlot(DisplaySlot.SIDEBAR);
             obj.setDisplayName(ChatColor.GOLD + "Money");
             Score score = obj.getScore(p.getName());
             score.setScore((int) econ.getBalance(p.getName()));
             p.setScoreboard(board);
-            this.players.add(p.getName());
-            if (this.players.contains(p.getName())) {
-                if(enable == true) {
+            players.add(p.getName());
+            if (players.contains(p.getName())) {
+                if (enable) {
                     Score score1 = obj.getScore(p);
                     score1.setScore((int) econ.getBalance(p.getName()));
                     p.setScoreboard(board);
-                }
-                else {
-                    board.resetScores(p.getName());
-                }
-            }
-        }
-    }
-
-    public void onDisable() {
-        super.onDisable();
-    }
-
-    @SuppressWarnings("deprecation")
-    @EventHandler
-    public void onJoin(PlayerJoinEvent e) {
-        for (Player p : Bukkit.getOnlinePlayers()) {
-            p.setScoreboard(Bukkit.getScoreboardManager().getNewScoreboard());
-            Scoreboard board;
-            board = Bukkit.getScoreboardManager().getNewScoreboard();
-            Objective obj = board.registerNewObjective(p.getName(),"dummy");
-            obj.setDisplaySlot(DisplaySlot.SIDEBAR);
-            obj.setDisplayName(ChatColor.GOLD + "Money");
-            Score score = obj.getScore(p.getName());
-            score.setScore((int) econ.getBalance(p.getName()));
-            p.setScoreboard(board);
-            this.players.add(p.getName());
-            if (this.players.contains(p.getName())) {
-                if(enable == true) {
-                    Score score1 = obj.getScore(p);
-                    score1.setScore((int) econ.getBalance(p.getName()));
-                    p.setScoreboard(board);
-                }
-                else {
+                } else {
                     board.resetScores(p.getName());
                 }
             }
@@ -124,8 +78,8 @@ public class SidebarEconomy extends JavaPlugin implements Listener {
 
     public boolean onCommand(CommandSender sender, Command cmd, String commandLabel, String[] args){
         if(cmd.getName().equalsIgnoreCase("side")){ 
-            if(enable == true) {sender.sendMessage("§A[SideBarEcon] §6Hide the sidebar.");}
-            if(enable == false) {sender.sendMessage("§A[SideBarEcon] §6Show the sidebar.");}
+            if (enable) sender.sendMessage("§A[SideBarEcon] §6Hide the sidebar.");
+            else        sender.sendMessage("§A[SideBarEcon] §6Show the sidebar.");
             enable = !enable;
             return true;
         }
